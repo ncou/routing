d6a195ca4f8f27b4ad1affa216ac508c0a4fae72
ncou <nicolas_courade@yahoo.fr>
2020-11-14 14:12:50 +0100

    update

--- src/Bootloader/RouteCollectionBootloader.php
+++ src/Bootloader/RouteCollectionBootloader.php
@@ -8,8 +8,6 @@ use Chiron\Core\Container\Bootloader\AbstractBootloader;
 use Chiron\Http\Config\HttpConfig;
 use Chiron\Routing\RouteCollection;
 
-// TODO : il faudrait plutot créer un package "chiron/http" qui aurait aussi en dépendance le package "chiron/router" et on déplacerait ce bootloader dans le package http (idem pour la classe HttpConfig qui serait dans le package http !!!!)
-
 // TODO : transformer cette classe en un ServiceProvider et dans le binding du RouteCollection on en profiterai pour aussi initialiser le basePath ???? non ????
 class RouteCollectionBootloader extends AbstractBootloader
 {
